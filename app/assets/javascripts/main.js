0c4978fc0fda1a8e1c482b3e6573583ea5681f19
= <javierpedrozaing@gmail.com>
2023-01-01 20:36:45 -0500

    Update login / register pages

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -6,7 +6,15 @@ $( document ).ready(function() {
     $('#navbar').toggleClass('navbar');
     $('#navbar').toggleClass('navbar-mobile');
     $('.mobile-nav-toggle').toggleClass('fa-bars');
-    $('.mobile-nav-toggle').toggleClass('fa-rectangle-xmark');    
+    $('.mobile-nav-toggle').toggleClass('fa-rectangle-xmark');
+  });
+
+  $('.navbar .dropdown > a').on('click', function(e){
+    if ($('#navbar').hasClass('navbar-mobile')){
+      e.preventDefault()
+      $(this).next("ul").toggleClass('dropdown-active');
+    }    
+    return true;
   });
    
 });
