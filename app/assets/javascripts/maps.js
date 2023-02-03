22f1bdd4721597e4f531d555a038e553b5db0cbd
= <javierpedrozaing@gmail.com>
2023-02-03 12:11:35 -0500

    Improve maps

--- app/assets/javascripts/maps.js
+++ app/assets/javascripts/maps.js
@@ -4,63 +4,88 @@
 $(document).ready(function() {
   getLocations();
 
-  function getLocations() {
-    Rails.ajax({
-      url: "/brokers_locations",
-      type: "get",
-      success: function(data) {     
-        debugger   
-        initMap(data)
-      },
-      error: function(data) {
-        console.error(data);
-      }
-    })
-  }
 });
+
+function getLocations() {
+  Rails.ajax({
+    url: "/brokers_locations",
+    type: "get",
+    success: function(data) {     
+      coordinates = data.map((data) => {
+        return {
+          coordinates: data.coordinates,
+          name: data.name,
+          photo: data.photo,
+          city: data.city
+        }
+      });    
+      //window.initMap = initMap();
+      initMap(coordinates);
+    },    
+    error: function(data) {
+      console.error(data);
+    }
+  })
+};
 
 
 function initMap(locations) {
-  
+  latitude = locations ? locations[0].coordinates[0] : ""
+  longitude = locations ? locations[0].coordinates[1] : ""
+
   const map = new google.maps.Map(document.getElementById("map"), {
     zoom: 12,
-    center: { lat: locations[0].coordinates[0], lng: locations[0].coordinates[1] },
+    center: { lat: latitude, lng:  longitude },
   });
 
-  const contentString = "";
- 
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const pos = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+        };
+        
+        map.setCenter(pos);
+      },
+      () => {
+        handleLocationError(true, infoWindow, map.getCenter());
+      }
+    );
+  } else {
+    // Browser doesn't support Geolocation
+    handleLocationError(false, infoWindow, map.getCenter());
+  }
+
   const image =
-    "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png";
-       
-    var marker, i;
+  "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png";
     
-    for (i = 0; i < locations.length; i++) {
+  var marker, i;
+  
+  for (i = 0; i < locations.length; i++) {
 
-      const infowindow = new google.maps.InfoWindow({
-        content: "<div>"
-        + "<div style='display: flex;align-items: center;justify-content: space-around;'><img style='width: 20%;' src='"+ locations[i].photo +"'></div>"
-        + "<div style='display: flex;align-items: center;justify-content: space-around;'>Hola Soy, " + locations[i].name +
-         " y estoy en la ciudad de " + locations[i].city + "</div></div>",
-      });
+    const infowindow = new google.maps.InfoWindow({
+      content: "<div>"
+      + "<div style='display: flex;align-items: center;justify-content: space-around;'><img style='width: 20%;' src='"+ locations[i].photo +"'></div>"
+      + "<div style='display: flex;align-items: center;justify-content: space-around;'>Hola Soy, " + locations[i].name +
+      " y estoy en la ciudad de " + locations[i].city + "</div></div>",
+    });
 
-
-      console.log(locations);
-      marker = new google.maps.Marker({
-        position: new google.maps.LatLng(locations[i].coordinates[0], locations[i].coordinates[1]),
-        map: map,
-        icon: image,
-      });
-      
-      google.maps.event.addListener(marker, 'click', (function(marker, i) {
-        return function() {
-          // infowindow.setContent(locations[i][0]);
-          infowindow.open(map, marker);
-        }
-      })(marker, i));
-    }
+    console.log(locations);
+    marker = new google.maps.Marker({
+      position: new google.maps.LatLng(locations[i].coordinates[0], locations[i].coordinates[1]),
+      map: map,
+      icon: image,
+    });
+    
+    google.maps.event.addListener(marker, 'click', (function(marker, i) {
+      return function() {
+        // infowindow.setContent(locations[i][0]);
+        infowindow.open(map, marker);
+      }
+    })(marker, i));
+  }  
 }
-
-window.initMap = initMap();
 
 // This example requires the Places library. Include the libraries=places
 // parameter when you first load the API. For example:
