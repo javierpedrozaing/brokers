7e874cb760865bf1cec965ad0f2d37031643cf93
= <javierpedrozaing@gmail.com>
2023-02-12 16:45:42 -0500

    remove unnecesary relation

--- app/models/referral.rb
+++ app/models/referral.rb
@@ -1,7 +1,6 @@
 class Referral < ApplicationRecord
   belongs_to :user, class_name: "User", foreign_key: "user_id"
   belongs_to :source, class_name: "Source", foreign_key: "source_id"
-  belongs_to :time_frame, class_name: "TimeFrame", foreign_key: "time_frame_id"
-  belongs_to :type_of_reference, class_name: "TypeOfReference", foreign_key: "type_of_ref_id"
+  belongs_to :time_frame, class_name: "TimeFrame", foreign_key: "time_frame_id"  
   has_many :transations, class_name: "Transaction"
 end
--- db/migrate/20230107021738_add_type_of_ref_to_referrals.rb
+++
@@ -1,5 +0,0 @@
-class AddTypeOfRefToReferrals < ActiveRecord::Migration[5.2]
-  def change
-    add_reference :referrals, :type_of_ref, foreign_key: true
-  end
-end
