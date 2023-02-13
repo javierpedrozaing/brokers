ca8c582aef7118096fe84c0e7a150d1e6f80be71
= <javierpedrozaing@gmail.com>
2023-02-12 20:49:24 -0500

    fix get locations

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,17 +8,25 @@ class PagesController < ApplicationController
   def get_brokers_locations
     geocoder = Geocoder
     brokers = Broker.all
+    
     brokers_coordinates = brokers.map do |br|
+      photo = br.user.photo.attached? ? url_for(br.user.photo) : ''
       {   
         broker_id: br.id,
         name: "#{br.user.first_name} #{br.user.last_name}",
         city: br.city,
-        photo: url_for(br.user.photo),
+        photo: photo,
         coordinates: geocoder.coordinates(br.full_address)
       }
     end
-    brokers_coordinates.compact! 
-    render json: brokers_coordinates.to_json
+
+    if brokers_coordinates
+      brokers_coordinates.compact! 
+      render json: brokers_coordinates.to_json
+    else 
+      render json: ""
+    end
+
   end
 
   def search_brokers
