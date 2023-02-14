18fbf038a22dca712567b6dadbb0ed7bdb1bf70b
= <javierpedrozaing@gmail.com>
2023-02-14 15:08:22 -0500

    fix

--- app/models/agent.rb
+++ app/models/agent.rb
@@ -1,5 +1,6 @@
 class Agent < ApplicationRecord
   belongs_to :user, class_name: "User", foreign_key: "user_id"    
+  belongs_to :broker, class_name: "Broker", foreign_key: "broker_id"    
   has_many :transations, class_name: "Transaction"
   has_many :clients, class_name: "Client"
 
