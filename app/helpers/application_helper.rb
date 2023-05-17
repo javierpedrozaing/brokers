970a783f744b97252cd75c697609dff7a731843c
= <javierpedrozaing@gmail.com>
2023-05-16 21:23:11 -0500

    fix get countries list

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,7 +7,7 @@ module ApplicationHelper
   BATHROOMS = [['Uno', 'uno'], ['Dos', 'dos'], ['Tres', 'tres'], ['Cuatro', 'cuatro']]
 
   def self.get_countries_states_and_cities
-    @countries = ApplicationController::countries_list.map{|c| [c['name'], c['iso2']]} unless ApplicationController::countries_list.nil?
+    @countries = ApplicationController::countries_list.map{|c| [c['name'], c['iso2']]} unless ApplicationController::countries_list.nil? || ApplicationController::countries_list.empty?
     states_list = ApplicationController::states_list_by_country(@current_country) unless @current_country.nil?    
     @states = states_list.nil? || states_list.length <= 1 ? [] : states_list.map{|c| [c['name'], c['iso2']]}
     cities_list = ApplicationController::cities_list(@current_country) unless @current_country.nil?
